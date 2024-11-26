f09ced965c9c88e692c77ba198dfd6967a90ade5
Shiqi Pang <shiqipam@gmail.com>
2024-11-25 20:18:14 -0600

    Time: 3 ms (94.64%) | Memory: 44.3 MB (77.68%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.java
+++ 945-snakes-and-ladders/snakes-and-ladders.java
@@ -1,34 +1,60 @@
 class Solution {
+    class State{
+        int index,step;
+        boolean snake;
+        State(int index,int step,boolean snake){
+            this.index = index;
+            this.step = step;
+            this.snake = snake;
+        }
+    }
+    boolean[] seen;
+    Queue<State> queue;
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        Pair<Integer, Integer>[] cells = new Pair[n * n + 1];
-        int label = 1;
-        Integer[] columns = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            columns[i] = i;
-        }
-        for (int row = n - 1; row >= 0; row--) {
-            for (int column : columns) {
-                cells[label++] = new Pair<>(row, column);
+        boolean toggle = true; // true col++  false col--
+        int index = 1;
+        int[] data = new int[n*n+1];
+    
+
+        // convert 2D array to 1D ordered array;
+        for(int r=n-1; r>=0; r--){
+            if(toggle){
+                for(int c=0; c<n; c++){
+                    data[index++] = board[r][c];
+                }
+            }else{
+                for(int c=n-1; c>=0; c--){
+                    data[index++] = board[r][c];
+                }
             }
-            Collections.reverse(Arrays.asList(columns));
+            toggle = !toggle;
         }
-        int[] dist = new int[n * n + 1];
-        Arrays.fill(dist, -1);
-        Queue<Integer> q = new LinkedList<Integer>();
-        dist[1] = 0;
-        q.add(1);
-        while (!q.isEmpty()) {
-            int curr = q.remove();
-            for (int next = curr + 1; next <= Math.min(curr + 6, n * n); next++) {
-                int row = cells[next].getKey(), column = cells[next].getValue();
-                int destination = board[row][column] != -1 ? board[row][column] : next;
-                if (dist[destination] == -1) {
-                    dist[destination] = dist[curr] + 1;
-                    q.add(destination);
+        // graph concept 
+        queue = new LinkedList<>();
+        queue.add(new State(1,0,false));
+        seen = new boolean[n*n+1];
+        seen[1] = true;
+        while(!queue.isEmpty()){
+            State cur = queue.remove();
+            for(int r = 1; r <= 6; r++){
+                int next = cur.index + r;
+                if(next > n*n)
+                    break;
+                if(data[next] != -1){
+                    next = data[next];
+                }    
+
+                if(next == n*n){
+                    return cur.step + 1;
+                }
+
+                if(!seen[next]){
+                    seen[next] = true;
+                    queue.add(new State(next,cur.step+1,false));
                 }
             }
         }
-        return dist[n * n];
+        return -1;
     }
 }
