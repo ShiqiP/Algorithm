69464da9fb772f250a4571f1ea1092baaa021382
Shiqi Pang <shiqipam@gmail.com>
2024-11-29 10:28:46 -0600

    Time: 1 ms (91.00%) | Memory: 48.2 MB (14.34%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,47 +1,24 @@
 class Solution {
-    boolean[] seen;
-    Map<Integer,List<Integer>> graph;
 
     public int findCircleNum(int[][] isConnected) {
         // find the number of connected conponents
         int n = isConnected.length;
-        // convert adjacency matrix to map
-        graph = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            if (!graph.containsKey(i)) {
-                graph.put(i, new ArrayList<>());
-            }
-            for (int j = i + 1; j < n; j++) {
-                // undirected graoh;
-                if (!graph.containsKey(j)) {
-                    graph.put(j, new ArrayList<>());
-                }
-                if (isConnected[i][j] == 1) {
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);
-                }
-
-            }
-        }
-        int ans = 0;
-        // seen to keep track of the node we visited
-         seen = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            if (!seen[i]) {
+        boolean[] seen = new boolean[n];
+        int ans=0;
+        for(int i=0; i<n; i++){
+            if(!seen[i]){
                 seen[i] = true;
-                ans++;
-                dfs(i);
+                ans ++;
+                dfs(i,seen,isConnected);
             }
         }
         return ans;
     }
-
-    public void dfs(int node) {
-        // we execute dfs to visit all the directly and indirectly connected node
-        for (int i : graph.get(node)) {
-            if (!seen[i]) {
-                seen[i] = true;
-                dfs(i);
+    public void dfs(int i, boolean[] seen, int[][] isConnected){
+        for(int j=0; j<isConnected.length; j++){
+            if(isConnected[i][j] == 1 && !seen[j] ){
+                seen[j] = true;
+                dfs(j,seen,isConnected);
             }
         }
     }
