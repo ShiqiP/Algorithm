f5899589266dd01e5d8b67280ea3e04ee97508df
Shiqi Pang <shiqipam@gmail.com>
2024-12-25 17:55:22 -0600

    Time: 0 ms (100.00%) | Memory: 50.6 MB (14.80%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -5,19 +5,15 @@ class Solution {
         // mid position of the nums
         // mid mid+1 mid-1
         // mid
+        // 2 2 5 7 7 8 8 right = 6 mid = 3
+        // 2 2 5 5 7 8 8 right = 6 mid = 3 left = 0
         int n = nums.length;
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            System.out.println("-----");
-            System.out.println("left: " + left);
-            System.out.println("right: " + right);
-            System.out.println(mid);
             if (mid + 1 >= n || mid - 1 < 0)
                 return nums[mid];
-            // 2 2 5 5 7 8 8 right = 6 mid = 3 left = 0
             if (nums[mid] == nums[mid - 1]) {
-                System.out.println("lll");
                 if(right-left<3) return nums[mid+1];
                 if ((mid - left+1) % 2 == 0) {
                     left = mid + 1;
@@ -25,8 +21,6 @@ class Solution {
                     right = mid - 2;
                 }
             } else if (nums[mid] == nums[mid + 1]) {
-                // 2 2 5 7 7 8 8 right = 6 mid = 3
-                System.out.println("rrr");
                 if(right-left<3) return nums[mid-1];
                 if ((right - mid + 1) % 2 == 0) {
                     right = mid - 1;
