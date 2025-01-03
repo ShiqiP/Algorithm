70bd9fbbd6ad5e41487da7583e630fb0fb00afcd
Shiqi Pang <shiqipam@gmail.com>
2025-01-03 17:43:09 -0600

    Time: 2 ms (32.99%) | Memory: 41.7 MB (12.73%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        // the question want me to split k subarray in which the sum of elements is minimized
+        // the sum of each subarray is larger than or equal to the average which is sum of nums divided by k
+
+        // largest sum 
+        int total = 0;
+        int left = 0;
+        for(int n : nums){
+            total += n;
+            left = Math.max(left,n);
+        }
+        int right = total;
+
+        int ans = 0;
+        while(left <= right){
+            int mid = left + (right - left )/2;
+
+            int sum = 0;
+            int chunks = 1;
+            for(int n : nums){
+                if(sum + n <= mid){
+                    sum += n;
+                }else{
+                    sum = n;
+                    chunks ++;
+                }
+            }
+
+            if(chunks <= k){
+                right = mid - 1;
+                ans = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return ans; 
+    }
+}
