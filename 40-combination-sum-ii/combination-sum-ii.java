c71f1f019dec763c252b4767563249772526fc10
Shiqi Pang <shiqipam@gmail.com>
2025-01-11 10:40:12 -0600

    Time: 3 ms (77.64%) | Memory: 43.6 MB (14.04%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,10 +1,7 @@
 class Solution {
-    Set<Integer> set;
-    int sum;
-
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        this.set = new HashSet<>();
+        Arrays.sort(candidates);
         backtracking(0, new ArrayList<>(), 0, candidates, target, ans);
         return ans;
     }
@@ -17,16 +14,15 @@ class Solution {
         }
         for (int i = index; i < candidates.length; i++) {
             boolean seen = false;
-            for (int j = 0; j < i; j++) {
-                if (candidates[j] == candidates[i] && !set.contains(j))
-                    seen = true;
-            }
+            // for (int j = 0; j < i; j++) {
+            //     if (candidates[j] == candidates[i] && !set.contains(j))
+            //         seen = true;
+            // }
+            if( i>index && candidates[i] == candidates[i-1]) continue;
             if (sum + candidates[i] <= target && !seen) {
                 cur.add(candidates[i]);
-                set.add(i);
                 backtracking(i + 1, cur, sum + candidates[i], candidates, target, ans);
                 cur.remove(cur.size() - 1);
-                set.remove(i);
             }
         }
     }
