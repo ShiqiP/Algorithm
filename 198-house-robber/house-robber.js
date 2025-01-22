27ef4db23da98af60cb9f40a17826a3865c45002
Shiqi Pang <shiqipam@gmail.com>
2025-01-22 15:24:03 -0600

    Time: 0 ms (100.00%) | Memory: 48.6 MB (86.40%) - LeetSync

--- 198-house-robber/house-robber.js
+++ 198-house-robber/house-robber.js
@@ -3,20 +3,16 @@
  * @return {number}
  */
 var rob = function (nums) {
-    // bottom-up
-    let dp = new Array(nums.length);
-    for(let i=0;i<nums.length;i++){
-        if(i == 0){
-            dp[0] = nums[0];
-            continue;
-        }
-        if(i == 1){
-            dp[1] = Math.max(nums[0],nums[1]);
-            continue;
-        }
-        dp[i] = Math.max(dp[i-1],dp[i-2] + nums[i]);
+    // bottom-up optimized space complexity
+    if(nums.length == 1) return nums[0]
+    let backOne =  Math.max(nums[0],nums[1]);
+    let backTwo = nums[0];
+
+    for(let i=2;i<nums.length;i++){
+        let temp = backOne;
+        backOne = Math.max(backOne,backTwo + nums[i]);
+        backTwo = temp;
     }
-    console.log(dp)
-    return dp[dp.length-1];
+    return backOne;
 
 };
