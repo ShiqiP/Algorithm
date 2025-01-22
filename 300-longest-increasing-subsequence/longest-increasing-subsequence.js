99a28c44766ca07a179ba5b4f1258bc88c70a3bd
Shiqi Pang <shiqipam@gmail.com>
2025-01-22 16:01:01 -0600

    Time: 421 ms (10.63%) | Memory: 52.3 MB (14.67%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    // bottom-up
+    const n = nums.length;
+    let memo = new Array(n).fill(0);
+    let max = 0;
+    for(let m in memo){
+        max = Math.max(max,dp(m));
+    }
+    console.log(memo)
+    return max;
+
+    function dp(index){
+        // base case
+        if(index == 0){
+            memo[0] = 1;
+            return 1;
+        }
+
+        if(memo[index] > 0)
+            return memo[index];
+                
+        memo[index] = 1
+        for(let i=index; i >= 0;i--){
+            if(nums[index] > nums[i]){
+                memo[index] = Math.max(1 + dp(i),memo[index]);
+            }
+        }
+
+        return memo[index];
+    }
+
+};
