3aea62384e1045f6ce2947e31567c7b9ea398714
Shiqi Pang <shiqipam@gmail.com>
2025-01-26 17:18:08 -0600

    Time: 83 ms (28.10%) | Memory: 55 MB (60.49%) - LeetSync

--- 322-coin-change/coin-change.js
+++ 322-coin-change/coin-change.js
@@ -10,25 +10,24 @@
 var coinChange = function (coins, amount) {
     // the fewest number of coins to make up certain amount;
     // from 0 - amount;
-    let memo = new Array(amount + 1).fill(0);
-    console.log(memo)
+    let memo = new Array(amount + 1).fill(amount + 1);
+    memo[0] = 0;
     dp(amount);
     return memo[amount];
 
     function dp(remain) {
         if (remain == 0) return 0;
         if (remain < 0) return -1;
-        if (memo[remain] !== 0) return memo[remain];
+        if (memo[remain] !== amount + 1) return memo[remain];
 
-        let minCount = Infinity
         for (let c of coins) {
             // recurrance relation
             // previous coins that make up remain-c
             const pre = dp(remain - c);
             if (pre === -1) continue;
-            minCount = Math.min(minCount, pre + 1);
+            memo[remain] = Math.min(memo[remain], pre + 1);
         }
-        memo[remain] = minCount === Infinity ? -1 : minCount;
+        memo[remain] = memo[remain] === amount + 1 ? -1 : memo[remain];
         return memo[remain];
     }
 };
