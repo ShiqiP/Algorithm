80fd198276bdef98bd25227362d0f19a50c5aa65
Shiqi Pang <shiqipam@gmail.com>
2025-02-01 19:28:03 -0600

    Time: 0 ms (100.00%) | Memory: 57.6 MB (83.72%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -6,8 +6,8 @@ var maxSubArray = function(nums) {
     let max = -Infinity;
     let sum = 0;
 
-    for(let n of nums){
-        sum = Math.max(sum + n , n);
+    for(let i = 0; i < nums.length; i++){
+        sum = Math.max(sum + nums[i] , nums[i]);
         max = Math.max(max, sum);
     }
 
