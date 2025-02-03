355b9edc3149bbac75aa2ff0152f2647f5614569
Shiqi Pang <shiqipam@gmail.com>
2025-02-03 11:45:38 -0600

    Time: 0 ms (100.00%) | Memory: 48.4 MB (90.53%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.js
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.js
@@ -2,15 +2,19 @@
  * @param {number} n
  * @return {number}
  */
-var tribonacci = function(n) {
+var tribonacci = function (n) {
     // time complexity O(n)
     // space complexity O(n)
-    let T = new Array(n+1).fill(0);
-    T[0] = 0;
-    T[1] = 1;
-    T[2] = 1;
-    for(let i = 3; i <= n; i++){
-        T[i] = T[i-1] + T[i-2] + T[i-3];
+    let memo = new Array(n+1).fill(-1);
+    memo[0] = 0;
+    memo[1] = 1;
+    memo[2] = 1;
+    let dp = (n)=>{
+        if(memo[n] !== -1){
+            return memo[n];
+        }
+        memo[n] = dp(n - 1) + dp(n - 2) + dp(n - 3)
+        return memo[n];
     }
-    return T[n];
+    return dp(n);
 };
