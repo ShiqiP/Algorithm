c22adc6dbe1d859ce6c32548aeca60f902840c1f
Shiqi Pang <shiqipam@gmail.com>
2025-02-07 17:58:38 -0600

    Time: 37 ms (64.13%) | Memory: 56.4 MB (31.13%) - LeetSync

--- 494-target-sum/target-sum.js
+++ 494-target-sum/target-sum.js
@@ -4,9 +4,7 @@
  * @return {number}
  */
 var findTargetSumWays = function (nums, target) {
-    // combination 
     const n = nums.length;
-    // let preSum = [0];
     let map = new Map();
     map.set(0, 1);
 
@@ -16,28 +14,11 @@ var findTargetSumWays = function (nums, target) {
             let curSum = key + nums[i];
             let curSum2 = key - nums[i];
             let ways = map.get(key)
-            if (next_map.has(curSum)) {
-                next_map.set(curSum, next_map.get(curSum) + ways)
-            } else {
-                next_map.set(curSum, ways)
-            }
-            if (next_map.has(curSum2)) {
-                next_map.set(curSum2, next_map.get(curSum2) + ways)
-            } else {
-                next_map.set(curSum2, ways)
-            }
-            // for (let el of curNum) {
-            //     let num = el + preSum[j];
-            //     curSum.push(num);
-            //     if (i === n - 1) {
-            //         if (num === target) {
-            //             ans++;
-            //         }
-            //     }
-            // }
+
+            next_map.set(curSum, next_map.has(curSum) ? next_map.get(curSum) + ways : ways)
+            next_map.set(curSum2, next_map.has(curSum2) ? next_map.get(curSum2) + ways : ways)
         }
         map = next_map;
     }
-    console.log(map)
     return map.has(target) ? map.get(target) : 0;
 };
