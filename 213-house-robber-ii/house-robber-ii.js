c9f4fd9b0de74dd70ee4d4a061c9dedf417b9231
Shiqi Pang <shiqipam@gmail.com>
2025-02-08 16:33:43 -0600

    Time: 2520 ms (5.41%) | Memory: 139.4 MB (5.11%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    let map = new Map();
+    let dp = (i, curSum, flag) => {
+        // base cases
+        if (i >= nums.length) {
+            return curSum;
+        }
+        if (flag === 0 && i === nums.length - 1) {
+            return curSum;
+        }
+        const key = i + ',' + curSum + ',' + flag;
+        if(map.has(key)){
+            return map.get(key);
+        }
+        const skip = dp(i + 1, curSum, flag);
+        const rob = dp(i + 2, curSum + nums[i], flag)
+        const ans = Math.max(skip, rob);
+        map.set(key,ans);
+        return ans;
+    }
+    let ans = nums.length === 1 ? nums[0] : Math.max(dp(0, 0, 0), dp(1, 0, 1));
+    console.log(map)
+    return ans;
+}
