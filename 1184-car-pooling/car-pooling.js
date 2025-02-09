4af048b3271a634a0acbce017d913ce1d4c036be
Shiqi Pang <shiqipam@gmail.com>
2025-02-09 17:48:14 -0600

    Time: 2 ms (92.11%) | Memory: 51.7 MB (70.00%) - LeetSync

--- 1184-car-pooling/car-pooling.js
+++ 1184-car-pooling/car-pooling.js
@@ -4,28 +4,24 @@
  * @return {boolean}
  */
 var carPooling = function(trips, capacity) {
-    let min = Infinity;
     let max = 0;
-    let total = 0; // represents how many passengers are in the car
     for(let trip of trips){
-        min = Math.min(trip[1], min);
         max = Math.max(trip[2], max);
     }
+    let arr = new Array(max + 1).fill(0);
     // current location that the car is in
-    for(let i = min; i <= max; i++){
-        for(let j = 0; j < trips.length; j++){
-            const trip = trips[j];
-            const passengers = trip[0];
-            const from = trip[1];
-            const to = trip[2];
-            if(i === from){
-                total += passengers;
-            }
-            if(i === to){
-                total -= passengers;
-            }
-        }
-        if(total > capacity){
+    for(let i = 0; i < trips.length; i++){
+        const num = trips[i][0];
+        const from = trips[i][1];
+        const to = trips[i][2];
+        arr[from] += num;
+        arr[to] -= num;
+    }
+    // passengers are accumulated
+    let passengers = 0;
+    for(let num of arr){
+        passengers += num;
+        if(passengers > capacity){
             return false;
         }
     }
