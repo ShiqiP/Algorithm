86d69f22abeb5ad9106d69fa101263710c474afd
Shiqi Pang <shiqipam@gmail.com>
2025-02-15 15:00:44 -0600

    Time: 7 ms (72.92%) | Memory: 56.8 MB (93.75%) - LeetSync

--- 370-range-addition/range-addition.js
+++ 370-range-addition/range-addition.js
@@ -4,14 +4,21 @@
  * @return {number[]}
  */
 var getModifiedArray = function(length, updates) {
-    let arr = new Array(length).fill(0); // edge-value
+    let arr = new Array(length).fill(0);
     for(let i = 0; i < updates.length; i++){
         const start = updates[i][0];
         const end = updates[i][1];
-        const value = updates[i][2];
-        for(let i = start; i <= end; i++){
-            arr[i] += value;
+        const val = updates[i][2];
+
+        arr[start] += val;
+        if(end + 1 < length){
+            arr[end + 1] -= val;
         }
+    }
+    let curVal = 0;
+    for(let i = 0; i < length; i++){
+        curVal += arr[i];
+        arr[i] = curVal;
     }
     return arr;
 };
