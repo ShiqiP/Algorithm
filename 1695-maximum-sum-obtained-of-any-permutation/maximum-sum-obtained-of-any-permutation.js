576eb060496585546df88b763d7be26f23603d27
Shiqi Pang <shiqipam@gmail.com>
2025-02-15 19:40:24 -0600

    Time: 144 ms (15.38%) | Memory: 77.7 MB (53.85%) - LeetSync

---
+++ 1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number[][]} requests
+ * @return {number}
+ */
+var maxSumRangeQuery = function(nums, requests) {
+    const n = nums.length;
+    let arr = new Array(nums.length).fill(0);
+    for(let req of requests){
+        arr[req[0]] += 1;
+        if(req[1] + 1 < nums.length){
+            arr[req[1] + 1] -= 1;
+        }
+    }
+    let overlaps = 0;
+    console.log(arr)
+    for(let i = 0; i < n; i++){
+        overlaps += arr[i];
+        arr[i] = overlaps
+    }
+    let ans = 0;
+    arr.sort((a,b) => b - a);
+    nums.sort((a,b) => b - a);
+    console.log(arr)
+    console.log(nums)
+    for(let i = 0; i < n; i++){
+        if(arr[i] <= 0) continue;
+        ans += arr[i] * nums[i];
+    }
+    return ans%(Math.pow(10,9) + 7);
+};
