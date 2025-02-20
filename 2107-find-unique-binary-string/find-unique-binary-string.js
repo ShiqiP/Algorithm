33fdd7ac35c77a99892f1b04810f900397e6772a
Shiqi Pang <shiqipam@gmail.com>
2025-02-19 21:41:50 -0600

    Time: 0 ms (100.00%) | Memory: 54 MB (16.90%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.js
+++ 2107-find-unique-binary-string/find-unique-binary-string.js
@@ -2,20 +2,24 @@
  * @param {string[]} nums
  * @return {string}
  */
-var findDifferentBinaryString = function(nums) {
+var findDifferentBinaryString = function (nums) {
     const len = nums[0].length;
     let backtrack = (cur) => {
-        if(cur.length === len){
-            return;
-        }
-        for(let i = 0; i <= 1; i++){
-            cur.push(i);
-            backtrack(cur);
-            if(cur.length === len && !nums.includes(cur.join(''))){
+        if (cur.length === len) {
+            if (!nums.includes(cur.join(''))) {
                 return cur.join('');
+            }
+            return ''
+        }
+        for (let i = 0; i <= 1; i++) {
+            cur.push(i);
+            let str = backtrack(cur);
+            if (str !== '') {
+                return str
             }
             cur.pop();
         }
+        return ''
     }
     return backtrack([])
 };
