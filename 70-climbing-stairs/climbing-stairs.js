5406d84283f8f5524f311ad5e3d4f7844710e8ee
Shiqi Pang <shiqipam@gmail.com>
2025-02-27 17:54:01 -0600

    Time: 4 ms (2.94%) | Memory: 54.1 MB (5.38%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if(n === 1) return 1;
+    let arr = new Array(n + 1);
+    arr[0] = 1;
+    arr[1] = 1;
+    for(let i = 2; i <= n; i++){
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    console.log(arr)
+    return arr[n];
+};
