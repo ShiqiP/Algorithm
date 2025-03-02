861d6f68b8a54e0f64e374ccf37de4209a2fa4e8
Shiqi Pang <shiqipam@gmail.com>
2025-03-01 18:09:52 -0600

    Time: 209 ms (58.33%) | Memory: 85 MB (8.33%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.js
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.js
@@ -4,25 +4,17 @@
  * @return {number}
  */
 var maxValueOfCoins = function (piles, k) {
-    let memo = new Array(piles.length).fill(0).map(() => new Array(k + 1).fill(-1));
-    let dp = (i, remain) => {
-        // base cases
-        if (remain == 0) return 0;
-        if (i == piles.length) return 0;
-        if (memo[i][remain] !== -1) {
-            return memo[i][remain];
+    const n = piles.length;
+    let memo = new Array(n + 1).fill(0).map(() => new Array(k + 1).fill(0));
+    for (let i = n - 1; i >= 0; i--) {
+        for (let remain = 1; remain <= k; remain++) {
+            memo[i][remain] = memo[i + 1][remain]; // skip this pile
+            let cur = 0;
+            for (let j = 0; j < Math.min(remain, piles[i].length); j++) {
+                cur += piles[i][j];
+                memo[i][remain] = Math.max(cur + memo[i + 1][remain - j - 1], memo[i][remain]);
+            }
         }
-        // skip
-        let res = dp(i + 1, remain);
-        let cur = 0;
-        // choose current pile 
-        for (let j = 0; j < Math.min(remain, piles[i].length); j++) {
-            cur += piles[i][j];
-            // skip
-            res = Math.max(res, cur + dp(i + 1, remain - 1 - j));
-        }
-        memo[i][remain] = res;
-        return res;
     }
-    return dp(0, k);
+    return memo[0][k];
 };
