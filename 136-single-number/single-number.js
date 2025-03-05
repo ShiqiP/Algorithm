fcf075de26c116426b05acb36681d1a8d7ba050a
Shiqi Pang <shiqipam@gmail.com>
2025-03-05 15:47:30 -0600

    Time: 2 ms (61.93%) | Memory: 55 MB (29.73%) - LeetSync

---
+++ 136-single-number/single-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let mask = 0;
+    for(const num of nums){
+        mask ^= num;
+    }
+    return mask;
+};
