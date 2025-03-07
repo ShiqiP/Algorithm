d4bfe99d2c2460f7c13ba96e35a3a2af728e7552
Shiqi Pang <shiqipam@gmail.com>
2025-03-07 17:28:22 -0600

    Time: 3 ms (87.56%) | Memory: 55.7 MB (22.28%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.js
+++ 252-meeting-rooms/meeting-rooms.js
@@ -3,18 +3,9 @@
  * @return {boolean}
  */
 var canAttendMeetings = function (intervals) {
-    let statusArr = [];
-    for (let i = 0; i < intervals.length; i++) {
-        const start = intervals[i][0];
-        const end = intervals[i][1];
-        statusArr.push([start, 1]);
-        statusArr.push([end, -1]);
-    }
-    statusArr.sort((a, b) => (a[0] - b[0] || a[1] - b[1]));
-    let attend = 0;
-    for (let i = 0; i < statusArr.length; i++) {
-        attend += statusArr[i][1];
-        if(attend > 1){
+    intervals.sort((a,b) => a[0] - b[0]);
+    for(let i = 1; i < intervals.length; i++){
+        if(intervals[i - 1][1] > intervals[i][0]){
             return false;
         }
     }
