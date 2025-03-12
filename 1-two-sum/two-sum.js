56653d2fceeb464c4b8afe11db351822a891e552
Shiqi Pang <shiqipam@gmail.com>
2025-03-11 20:08:39 -0500

    Time: 2 ms (75.08%) | Memory: 54 MB (30.95%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,22 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let ans = [];
     let map = new Map();
     for(let i = 0; i < nums.length; i++){
-        let indexArray = map.has(nums[i]) ? map.get(nums[i]) : [];
-        map.set(nums[i], [...indexArray, i]);
-    }
-    for(let value of map.keys()){
-        const curIndices = map.get(value);
-        const curIndex = curIndices.shift(); 
-        const diff = target - value;
-        if(map.has(diff) && map.get(diff) > 0){
-            const diffIndices = map.get(diff)
-            ans = [curIndex, diffIndices.shift()];
-            return ans;
+        if(map.has(target - nums[i])){
+            return [map.get(target - nums[i]), i];
         }
-        curIndices.unshift(curIndex);
+        map.set(nums[i], i);
     }
-    return ans;
+    return [];
 };
