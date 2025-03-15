4b5dbff847d77779626687652ca6ee45fcf8e2e0
Shiqi Pang <shiqipam@gmail.com>
2025-03-15 16:52:26 -0500

    Time: 2 ms (78.77%) | Memory: 57.7 MB (20.32%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} graph
+ * @return {boolean}
+ */
+var isBipartite = function (graph) {
+    let set = new Array(graph.length).fill(-1);
+    for (let i = 0; i < graph.length; i++) {
+        if(set[i] !== -1) continue;
+        let stack = [];
+        stack.push(i);
+        set[i] = 0;
+        while (stack.length !== 0) {
+            const node = stack.pop();
+            for (let adjacent of graph[node]) {
+                if (set[adjacent] === -1) {
+                    set[adjacent] = (set[node] + 1) % 2; // -1 -> 0  1->0  0->1
+                    stack.push(adjacent);
+                } else if (set[adjacent] === set[node]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+};
