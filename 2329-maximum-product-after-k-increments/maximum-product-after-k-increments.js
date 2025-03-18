5bc4167ec0a013e231bb7862d7f3f9f358b1d817
Shiqi Pang <shiqipam@gmail.com>
2025-03-18 12:10:37 -0500

    Time: 144 ms (53.33%) | Memory: 78.8 MB (26.67%) - LeetSync

--- 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.js
+++ 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.js
@@ -5,92 +5,47 @@
  */
 var maximumProduct = function (nums, k) {
     const MOD = Math.pow(10, 9) + 7;
-    let heap = new MinHeap(nums);
+    let minHeap = [...nums];
+    heapyfy(minHeap);
     let i = 0;
     while (i < k) {
-        let least = heap.poll() + 1;
-        heap.add(least);
+        // let num = minHeap.shift();
+        // minHeap.push(num + 1);
+        minHeap[0] = minHeap[0] + 1;
+        heapyfyUp(minHeap, 0);
         i++;
     }
     let ans = 1;
-    while (!heap.isEmpty()) {
-        ans = (ans * heap.poll()) % MOD;
+    for (let num of minHeap) {
+        ans = (ans * num) % MOD;
     }
-    return ans;
+    return ans % MOD;
+
+    function heapyfy(heap) {
+        for (let i = Math.floor(heap.length / 2); i >= 0; i--) {
+            heapyfyUp(heap, i)
+            // let j = i;
+            // let k = getMinChild(heap, i);
+            // while (k !== 0) {
+            //     [heap[j], heap[k]] = [heap[k], heap[j]];
+            //     j = k;
+            //     k = getMinChild(heap, j);
+            // }
+        }
+    }
+    function heapyfyUp(heap, i) {
+        let j = i;
+        const leftChild = 2 * i + 1;
+        const rightChild = 2 * i + 2;
+        if (leftChild < heap.length && heap[leftChild] < heap[j]) {
+            j = leftChild;
+        }
+        if (rightChild < heap.length && heap[rightChild] < heap[j]) {
+            j = rightChild;
+        }
+        if (i !== j) {
+            [heap[j], heap[i]] = [heap[i], heap[j]];
+            heapyfyUp(heap, j)
+        }
+    }
 };
-
-class MinHeap {
-    constructor(heap = []) {
-        this.heap = [];
-        heap.forEach(num => this.add(num)); // Fix: Use an arrow function to preserve `this`
-    }
-
-    add(num) {
-        this.heap.push(num);
-        this.heapifyUp();
-    }
-
-    peek() {
-        return this.heap[0];
-    }
-
-    poll() {
-        if (this.heap.length === 0) return undefined;
-        const top = this.heap[0];
-        this.heap[0] = this.heap[this.heap.length - 1];
-        this.heap.pop();
-        this.heapifyDown();
-        return top;
-    }
-
-    isEmpty() {
-        return this.heap.length === 0;
-    }
-
-    heapifyUp() {
-        let nodeIndex = this.heap.length - 1;
-        while (nodeIndex > 0) {
-            const parentIndex = this.getParentIndex(nodeIndex);
-            if (this.heap[parentIndex] <= this.heap[nodeIndex]) break;
-            this.swap(parentIndex, nodeIndex);
-            nodeIndex = parentIndex;
-        }
-    }
-
-    heapifyDown() {
-        let nodeIndex = 0;
-        while (true) {
-            const leftChildIndex = this.getLeftChildIndex(nodeIndex);
-            const rightChildIndex = this.getRightChildIndex(nodeIndex);
-            let smallest = nodeIndex;
-
-            if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallest]) {
-                smallest = leftChildIndex;
-            }
-            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallest]) {
-                smallest = rightChildIndex;
-            }
-            if (smallest === nodeIndex) break;
-            this.swap(nodeIndex, smallest);
-            nodeIndex = smallest;
-        }
-    }
-
-    getParentIndex(childIndex) {
-        return Math.floor((childIndex - 1) / 2);
-    }
-
-    getLeftChildIndex(parentIndex) {
-        return 2 * parentIndex + 1;
-    }
-
-    getRightChildIndex(parentIndex) {
-        return 2 * parentIndex + 2;
-    }
-
-    swap(indexOne, indexTwo) {
-        const tmp = this.heap[indexTwo];
-        this.heap[indexTwo] = this.heap[indexOne];
-        this.heap[indexOne] = tmp;
-    }
-}
