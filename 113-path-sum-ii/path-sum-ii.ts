c0055483c9b36e1074388dd044b22f7f8bbaacf1
Shiqi Pang <shiqipam@gmail.com>
2025-03-24 16:30:28 -0500

    Time: 5 ms (50.89%) | Memory: 66.5 MB (20.98%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function pathSum(root: TreeNode | null, targetSum: number): number[][] {
+    let ans: number[][] = [];
+    function dfs(root: TreeNode | null, sum: number, list: number[]) {
+        if (root === null) return;
+
+        if (root.left === null && root.right === null) {
+            if (sum + root.val === targetSum) {
+                ans.push([...list, root.val]);
+            }
+            return;
+        }
+        let curSum = sum + root.val;
+        let curList = [...list, root.val];
+        if (root.left !== null) {
+            dfs(root.left, curSum, curList);
+        }
+        if (root.right !== null) {
+            dfs(root.right, curSum, curList);
+        }
+        return;
+    }
+    dfs(root, 0, []);
+    return ans;
+};
