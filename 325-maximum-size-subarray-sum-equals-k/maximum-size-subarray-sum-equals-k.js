56ace78aa1bc1463f539b4c31eefb19ed8052351
Shiqi Pang <shiqipam@gmail.com>
2025-04-17 11:19:48 -0500

    Time: 86 ms (43.43%) | Memory: 87.4 MB (93.94%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.js
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.js
@@ -11,26 +11,22 @@ var maxSubArrayLen = function (nums, k) {
     const n = nums.length;
     let preSumArr = [];
     let map = new Map();
-    preSumArr[0] = nums[0];
-    map.set(preSumArr[0], 0);
-
-    for (let i = 1; i < n; i++) {
-        preSumArr[i] = preSumArr[i - 1] + nums[i];
+    let ans = 0;
+    for (let i = 0; i < n; i++) {
+        if(i === 0)
+            preSumArr[0] = nums[0];
+        else
+            preSumArr[i] = preSumArr[i - 1] + nums[i];
+        if (map.has(preSumArr[i] - k)) {
+            ans = Math.max(i - map.get(preSumArr[i] - k), ans);
+        }
+        if (preSumArr[i] === k) {
+            ans = Math.max(i + 1, ans);
+        }
         if (!map.has(preSumArr[i])) {
             map.set(preSumArr[i], i);
         }
     }
-    // preSumArr = [1,0,5,3,6]
-    // map = {1:0, 0:1, 5:2, 3:3, 6:4 } 
-    //          3-1 + 1
-    let ans = 0;
-    for (let i = n - 1; i >= 0; i--) {
-        if (map.has(preSumArr[i] - k)) {
-            ans = Math.max(i - map.get(preSumArr[i] - k), ans);
-        }
-        if(preSumArr[i] === k){
-            ans = Math.max(i + 1, ans);
-        }
-    }
+
     return ans;
 };
