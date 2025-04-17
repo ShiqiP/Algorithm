e519eadc2a7a307ff9ee3c07188a8a14b3abe3b7
Shiqi Pang <shiqipam@gmail.com>
2025-04-17 11:22:06 -0500

    Time: 71 ms (90.91%) | Memory: 92.1 MB (42.42%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.js
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.js
@@ -9,22 +9,19 @@ var maxSubArrayLen = function (nums, k) {
     // preSumArr map preSum -> index;
     // traverse preSumArr if(map.has(preSum - k)) curIndex - map.get(preSum - k)[0];
     const n = nums.length;
-    let preSumArr = [];
+    let preSum = 0;
     let map = new Map();
     let ans = 0;
     for (let i = 0; i < n; i++) {
-        if(i === 0)
-            preSumArr[0] = nums[0];
-        else
-            preSumArr[i] = preSumArr[i - 1] + nums[i];
-        if (map.has(preSumArr[i] - k)) {
-            ans = Math.max(i - map.get(preSumArr[i] - k), ans);
+        preSum += nums[i]
+        if (map.has(preSum - k)) {
+            ans = Math.max(i - map.get(preSum - k), ans);
         }
-        if (preSumArr[i] === k) {
+        if (preSum === k) {
             ans = Math.max(i + 1, ans);
         }
-        if (!map.has(preSumArr[i])) {
-            map.set(preSumArr[i], i);
+        if (!map.has(preSum)) {
+            map.set(preSum, i);
         }
     }
 
