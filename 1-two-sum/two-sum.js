ff0c189c26cb62d250d7b3ac6e6c3486082f1095
Shiqi Pang <shiqipam@gmail.com>
2026-04-30 22:05:48 -0500

    Time: 5 ms (43.80%) | Memory: 57.2 MB (8.85%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,11 +5,15 @@
  */
 var twoSum = function(nums, target) {
     let map = new Map();
+    let ans = new Array(2);
     for(let i = 0; i < nums.length; i++){
-        if(map.has(nums[i])){
-            return [map.get(nums[i]), i];
+        let aim = target - nums[i];
+        if(map.has(aim)){
+            ans[0] = i;
+            ans[1] = map.get(aim);
+        }else{
+            map.set(nums[i], i)
         }
-        map.set(target - nums[i], i);
     }
-    return [];
+    return ans;
 };
