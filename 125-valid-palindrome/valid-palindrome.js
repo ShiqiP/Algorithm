42a6626570efd97e12c499723add5bbbb2f451fd
Shiqi Pang <shiqipam@gmail.com>
2026-04-30 22:52:10 -0500

    Time: 7 ms (36.42%) | Memory: 59.9 MB (32.84%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -7,24 +7,25 @@ var isPalindrome = function(s) {
     // two pointer 
     let left = 0, right = s.length - 1;
     while(left < right){
-        if(checkAlphabet(s[left]) && checkAlphabet(s[right])){
-            console.log(left, right)
-            if(s[left].toLowerCase() != s[right].toLowerCase()){
-                return false;
-            }
+        while(left < right && !checkAlphabet(s[left])){
             left++;
-            right--;
-        }else if(!checkAlphabet(s[left])){
-            left++;
-        }else if(!checkAlphabet(s[right])){
+        }
+
+        while(left < right && !checkAlphabet(s[right])){
             right--;
         }
+        if(s[left].toLowerCase() != s[right].toLowerCase()){
+            return false;
+        }
+        left++;
+        right--;
+      
     }
 
     function checkAlphabet(c){
         const code = c.toLowerCase().charCodeAt(0);
-        return ('a'.charCodeAt() <= code && code <= 'z'.charCodeAt()) ||( code >= '0'.charCodeAt() && code <= '9'.charCodeAt());
+        return ('a'.charCodeAt() <= code && code <= 'z'.charCodeAt()) 
+        ||( code >= '0'.charCodeAt() && code <= '9'.charCodeAt());
     }
-    console.log(left, right)
     return true;
 };
