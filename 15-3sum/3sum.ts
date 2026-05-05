9fe94d6197c250475fa5c58f7a36de26d7e7d8c7
Shiqi Pang <shiqipam@gmail.com>
2026-05-05 17:18:33 -0500

    Time: 375 ms (14.14%) | Memory: 71.2 MB (97.39%) - LeetSync

--- 15-3sum/3sum.ts
+++ 15-3sum/3sum.ts
@@ -1,25 +1,24 @@
 function threeSum(nums: number[]): number[][] {
-    // two pointer and one fixed
-    let dups = new Set<number>();
-    let seen = new Map<number, number>();
-    let res = new Set<string>();
-    let i = 0;
-    while(i < nums.length){
-        if(dups.has(nums[i])) {
-            i++;
-            continue;
-        }
-        dups.add(nums[i]);
-        let j = i + 1;
-        while(j < nums.length){
+    // 1. sort the array
+    nums.sort(); // ascending order
+    let ans: number[][] = [];
+    // 2. for i skip if the current value === previous.
+    for (let i = 0; i < nums.length && nums[i] <= 0; i++) {
+        // (prevent duplicate value of index i)
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let map = new Set();
+        for (let j = i + 1; j < nums.length; j++) {
             const complement = -nums[i] - nums[j];
-            if(seen.has(complement) && seen.get(complement) === i){
-                res.add(JSON.stringify([complement, nums[i],nums[j]].sort()));
+            if (map.has(complement)) {
+                ans.push([complement, nums[i], nums[j]]);
+                // 3. for j current === next
+                // prevent duplicate value of index j
+                while (j + 1 < nums.length && nums[j] === nums[j + 1]) {
+                    j++;
+                }
             }
-            seen.set(nums[j], i);
-            j++;
+            map.add(nums[j]);
         }
-        i++;
     }
-    return Array.from(res).map(el => JSON.parse(el));
+    return ans;
 };
