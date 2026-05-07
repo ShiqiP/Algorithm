030bb96493946dbd1e3fb2357adbb6e4d2c6ed90
Shiqi Pang <shiqipam@gmail.com>
2026-05-06 23:04:47 -0500

    Time: 9 ms (32.06%) | Memory: 60.9 MB (20.31%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.ts
+++ 42-trapping-rain-water/trapping-rain-water.ts
@@ -6,18 +6,29 @@ function trap(height: number[]): number {
     // Math.min(left, right) - height[i]
     // 1. iterate height
     // need to be in the middle to store water
+    // 2. DP to store the leftmax and rightmax of each position with O(2N);
+    let rightMax = new Array(n).fill(0);
+    let leftMax = new Array(n).fill(0);
+    leftMax[0] = height[0];
     for (let i = 1; i < n; i++) {
-        let leftMax = 0, rightMax = 0;
+        leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+    }
+    rightMax[n - 1] = height[n - 1];
+    for (let i = n - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+    }
+    for (let i = 1; i < n; i++) {
+        // let leftMax = 0, rightMax = 0;
         // find the leftMax, rightMax
-        for (let j = i; j >= 0; j--) {
-            leftMax = Math.max(height[j], leftMax);
-        }
-        for(let k = i; k < n; k++){
-            rightMax = Math.max(height[k], rightMax);
-        }
+        // for (let j = i; j >= 0; j--) {
+        //     leftMax = Math.max(height[j], leftMax);
+        // }
+        // for (let k = i; k < n; k++) {
+        //     rightMax = Math.max(height[k], rightMax);
+        // }
         // calculate the water of each position
         // only store the water of the minimum height
-        ans += Math.min(leftMax, rightMax) - height[i];
+        ans += Math.min(leftMax[i], rightMax[i]) - height[i];
     }
     return ans;
 }
