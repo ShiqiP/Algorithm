110d4f3d3d42b93246c2a0892e48ae662afca75a
Shiqi Pang <shiqipam@gmail.com>
2026-05-08 19:50:22 -0500

    Time: 1 ms (74.83%) | Memory: 56.5 MB (46.42%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.ts
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.ts
@@ -1,9 +1,9 @@
-function maxProfit(prices: number[]): number {
-    let profit = 0; 
-    for(let i = 1; i < prices.length; i++){
-        if(prices[i] > prices[i - 1]){
-            profit += prices[i] - prices[i - 1];
+function maxProfit(prices: number[]): number {  
+    let ans = 0; // sum of the profit
+    for(let i = 0; i < prices.length - 1; i++){
+        if(prices[i + 1] > prices[i]){
+            ans += prices[i + 1] - prices[i];
         }
     }
-    return profit;
+    return ans;
 };
