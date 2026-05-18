77b3e8f3a26a0b34592e24dd88c34f742faee044
Shiqi Pang <shiqipam@gmail.com>
2026-05-18 12:18:30 -0500

    Time: 23 ms (75.00%) | Memory: 58.3 MB (87.84%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.ts
+++ 76-minimum-window-substring/minimum-window-substring.ts
@@ -6,37 +6,42 @@ function minWindow(s: string, t: string): string {
     let l = 0, r = 0;
     let tMap = new Map<string, number>();
     let sMap = new Map<string, number>();
-    let valid = false;
+    let required = 0;
+    let formed = 0;
+    let ans = new Array(2).fill(null);
+
     for (let i = 0; i < t.length; i++) {
         let freq = 1 + (tMap.get(t[i]) || 0);
         tMap.set(t[i], freq);
     }
-    let ans = new Array(2).fill(null);
+    required = tMap.size;
+
     while (r < s.length) {
         let freq = 1 + (sMap.get(s[r]) || 0);
         sMap.set(s[r], freq);
 
-        while (isValid(sMap, tMap)) {
-            if (valid === false || (ans[1] - ans[0] > r - l)) {
+        if(tMap.has(s[r]) && freq === tMap.get(s[r])){
+            formed ++;
+        }
+        
+        // valid 
+        while (l <= r && required === formed) {
+
+            if (ans[0] === null || (ans[1] - ans[0] > r - l)) {
                 ans[0] = l;
                 ans[1] = r;
             }
-            valid = true;
+
             sMap.set(s[l], sMap.get(s[l]) - 1);
+            if(tMap.has(s[l]) && sMap.get(s[l]) < tMap.get(s[l])){
+                formed--;
+            }
+
             l++;
         }
 
         r++;
     }
-    function isValid(aMap, bMap): boolean {
-        let valid = true;
-        for (const key of bMap.keys()) {
-            if (!aMap.get(key) || aMap.get(key) < bMap.get(key)) {
-                valid = false;
-                break;
-            }
-        }
-        return valid;
-    }
+
     return ans[0] === null ? "" : s.substring(ans[0], ans[1] + 1);
 };
