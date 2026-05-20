fa6d1fa88c0ad70e1e8cc9c942dbba2d18c9e06c
Shiqi Pang <shiqipam@gmail.com>
2026-05-20 15:30:00 -0700

    Time: 1625 ms (5.10%) | Memory: 63.5 MB (77.66%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.ts
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.ts
@@ -1,25 +1,19 @@
 function subarraySum(nums: number[], k: number): number {
     // pre sum 
-    // interate the presum
-    let presum: number[] = [];
-    presum[0] = 0
-    for (let i = 1; i <= nums.length; i++) {
-        presum[i] = presum[i - 1] + nums[i - 1];
-    }
-    let i = 0, ans = 0;
-    while (i < nums.length) {
-        let j = i + 1;
-        while (j <= nums.length) {
-            if (presum[j] - presum[i] === k) ans++;
-            j++;
-        }
-        i++
-    }
 
-    // [1, 3, 6]
-    // sum  j  i  ans
-    // 1    0  0  0
-    // 3    0  1  1
-    // 6,5,3   0,1,2   2 
+    // [1,3,6]
+    let ans = 0;
+    let presum:number[] = []
+    presum[0] = 0;
+    for(let i = 1; i <= nums.length; i++){
+        presum[i] = presum[i - 1] + nums[i - 1]; 
+    }
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j <= nums.length; j++){
+            if(presum[j] - presum[i] === k){
+                ans++;
+            }
+        }
+    }
     return ans;
 };
