194d1a714fa42a5fd7e0d36a588ea9b68c696149
Shiqi Pang <shiqipam@gmail.com>
2026-06-08 10:50:43 -0700

    Time: 0 ms (100.00%) | Memory: 58.3 MB (69.70%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let node = head;
+    let preNode = null;
+    while (node) {
+        let nextNode = node.next;
+
+        // reverse
+        node.next = preNode;
+
+        // traverse
+        preNode = node;
+        node = nextNode;
+    }
+    return preNode;
+};
