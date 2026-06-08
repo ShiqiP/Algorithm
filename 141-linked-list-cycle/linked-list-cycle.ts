d9b367178097b7496798b7f6566c23a3ef4f7493
Shiqi Pang <shiqipam@gmail.com>
2026-06-08 12:06:23 -0700

    Time: 52 ms (36.47%) | Memory: 58.5 MB (29.77%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    // fast node, slow node
+    let fast: ListNode = head;
+    let slow: ListNode = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast && fast === slow) {
+            return true;
+        }
+
+    }
+    return false;
+};
