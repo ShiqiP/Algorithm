a05c01f4f4c929af6cf1e480ad386c156d6c6af2
Shiqi Pang <shiqipam@gmail.com>
2026-06-16 15:20:44 -0700

    Time: 0 ms (100.00%) | Memory: 58.6 MB (41.81%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -11,21 +11,15 @@
  */
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    let head: ListNode = new ListNode(0);
-    let pre = head;
-    while (list1 && list2) {
-        if (list1.val < list2.val) {
-            pre.next = list1;
-            list1 = list1.next;
-        } else {
-            pre.next = list2;
-            list2 = list2.next;
-        }
-        pre = pre.next;
+    // recruision
+    if(list1 === null) return list2;
+    if(list2 === null) return list1;
+
+    if(list1.val < list2.val){
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    }else{
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
-
-    if (list1) pre.next = list1;
-    if (list2) pre.next = list2;
-
-    return head.next;
 };
