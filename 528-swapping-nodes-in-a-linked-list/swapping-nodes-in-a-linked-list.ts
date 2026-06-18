9b5a8c8b77d22fad09a84be7cc499a10bfd5d8b5
Shiqi Pang <shiqipam@gmail.com>
2026-06-18 16:45:04 -0700

    Time: 30 ms (9.68%) | Memory: 95.5 MB (22.58%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.ts
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.ts
@@ -14,27 +14,17 @@ function swapNodes(head: ListNode | null, k: number): ListNode | null {
     let stack: Array<ListNode> = [];
     let node = head;
     let node1 = null, node2 = null;
-    let count = 1;
+
+
     while (node) {
         stack.push(node);
-        if (count === k) {
-            node1 = node;
-        }
         node = node.next;
-        count++;
     }
 
-    count = k - 1;
-    while (count > 0) {
-        stack.pop();
-        count--;
-    }
+    node1 = stack[k - 1];
+    node2 = stack[stack.length - k];
 
-    node2 = stack[stack.length - 1];
-
-    let temp = node1.val;
-    node1.val = node2.val;
-    node2.val = temp;
+    [node1.val, node2.val] = [node2.val, node1.val];
 
     return head;
 };
