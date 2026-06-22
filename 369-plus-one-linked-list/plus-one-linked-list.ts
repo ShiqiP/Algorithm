793f3a5d1799331c60f5183666b34d701cac8d4d
Shiqi Pang <shiqipam@gmail.com>
2026-06-22 11:18:53 -0700

    Time: 43 ms (50.00%) | Memory: 55.6 MB (50.00%) - LeetSync

--- 369-plus-one-linked-list/plus-one-linked-list.ts
+++ 369-plus-one-linked-list/plus-one-linked-list.ts
@@ -11,22 +11,39 @@
  */
 
 function plusOne(head: ListNode | null): ListNode | null {
-    let stack: Array<ListNode> = [];
-    let node: ListNode = head;
+    // let stack: Array<ListNode> = [];
+    // let node: ListNode = head;
 
-    while (node) {
-        stack.push(node);
-        node = node.next;
+    // while (node) {
+    //     stack.push(node);
+    //     node = node.next;
+    // }
+
+    // let flag: boolean = true;
+
+    // while (flag && stack.length > 0) {
+    //     let node = stack.pop();
+    //     flag = (node.val + 1) > 9 ? true : false;
+    //     node.val = flag ? 0 : node.val + 1;
+    // }
+    function addOne(node: ListNode): number {
+        if (node.next === null) {
+            const sum = node.val + 1;
+            node.val = sum % 10;
+            const carry = sum > 9 ? 1 : 0;
+            return carry;
+        }
+
+        let carry = addOne(node.next);
+
+        const sum = carry + node.val;
+        node.val = sum % 10;
+
+        carry = sum > 9 ? 1 : 0;
+        return carry;
     }
 
-    let flag: boolean = true;
+    const carry = addOne(head);
 
-    while (flag && stack.length > 0) {
-        let node = stack.pop();
-        flag = (node.val + 1) > 9 ? true : false;
-        node.val = flag ? 0 : node.val + 1;
-    }
-
-
-    return flag ? new ListNode(1, head) : head;
+    return carry >= 1 ? new ListNode(1, head) : head;
 };
