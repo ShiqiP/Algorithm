c69f24d71eefc8dc6fe6d31e48b429f218e0c393
Shiqi Pang <shiqipam@gmail.com>
2026-06-23 23:52:45 -0700

    Time: 1 ms (100.00%) | Memory: 46 MB (98.52%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -14,47 +14,36 @@ class Solution {
         l1 = reverse(l1);
         l2 = reverse(l2);
 
-        int carry = 0;
         ListNode dummy = new ListNode(-1);
         ListNode l3 = dummy;
-        while (l1 != null && l2 != null) {
-            int sum = l1.val + l2.val + carry;
+        int carry = 0;
+        int sum = 0;
+
+        while (l1 != null || l2 != null) {
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            // 7 2 4 3 
+            //   5 6 4
             int value = sum % 10;
             carry = sum / 10;
+            sum = carry;
 
             l3.next = new ListNode(value);
             l3 = l3.next;
 
-            l1 = l1.next;
-            l2 = l2.next;
+        }
+        if (carry > 0) {
+            l3.next = new ListNode(carry);
         }
 
-        while (l1 != null) {
-            int sum = l1.val + carry;
-            int value = sum % 10;
-            carry = sum / 10;
-
-            l3.next = new ListNode(value);
-            l3 = l3.next;
-            l1 = l1.next;
-        }
-
-        while (l2 != null) {
-            int sum = l2.val + carry;
-            int value = sum % 10;
-            carry = sum / 10;
-
-            l3.next = new ListNode(value);
-            l3 = l3.next;
-            l2 = l2.next;
-        }
-
-        while (carry > 0) {
-            int value = carry % 10;
-            carry = carry / 10;
-            l3.next = new ListNode(value);
-            l3 = l3.next;
-        }
         return reverse(dummy.next);
     }
 
