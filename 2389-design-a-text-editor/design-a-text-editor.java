37fbb19dbd362ac079258efe3b0baded45c5a80f
Shiqi Pang <shiqipam@gmail.com>
2026-07-12 09:53:49 -0700

    Time: 214 ms (10.49%) | Memory: 93 MB (83.94%) - LeetSync

--- 2389-design-a-text-editor/design-a-text-editor.java
+++ 2389-design-a-text-editor/design-a-text-editor.java
@@ -24,30 +24,24 @@ class TextEditor {
     }
 
     public String cursorLeft(int k) {
-        // System.out.println("-----cursorLeft----");
-        // System.out.println(this.cursor);
-        // System.out.println(k);
-        // System.out.println(this.cursor - k);
+
         this.cursor = Math.max(0, this.cursor - k);
-        int start = Math.max(0, this.cursor - 10);
-        int end = this.cursor;
-        // System.out.println("start:" + start + "; end:" + end);
-        // System.out.println(builder.toString());
-        return builder.substring(start, end);
+
+        return leftSubString();
+
     }
 
     public String cursorRight(int k) {
-        // System.out.println("-----cursorRight----");
-        // System.out.println(this.cursor);
-        // System.out.println(k);
-        // System.out.println(this.cursor + k);
+
         this.cursor = Math.min(this.cursor + k, this.builder.length());
-        // int start = this.cursor;
-        // int end = Math.min(this.cursor + 10, this.builder.length());
+
+        return leftSubString();
+    }
+
+    private String leftSubString() {
         int start = Math.max(0, this.cursor - 10);
         int end = this.cursor;
-        // System.out.println("start:" + start + "; end:" + end);
-        // System.out.println(builder.toString());
+
         return builder.substring(start, end);
     }
 }
