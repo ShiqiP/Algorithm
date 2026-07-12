14b59ba5273b65b6059079fdb0f3b62138d32b7e
Shiqi Pang <shiqipam@gmail.com>
2026-07-12 11:45:45 -0700

    Time: 0 ms (100.00%) | Memory: 42.2 MB (33.45%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,18 +1,16 @@
 class Solution {
-    private Map<Integer, Integer> map;
-    public Solution(){
-        map = new HashMap<>();
-    }
-    public int fib(int n) {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
-        if(map.containsKey(n)) return map.get(n);
 
-        int ans = fib(n - 1) + fib(n - 2);
-        map.put(n, ans);
-        return ans;
+    public int fib(int n) {
+        if(n == 0) return 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 
 }
