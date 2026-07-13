3b623ec545d10e0e46561114157842c6d22cb9bf
Shiqi Pang <shiqipam@gmail.com>
2026-07-13 13:27:39 -0700

    Time: 8 ms (76.20%) | Memory: 70.8 MB (87.67%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -2,31 +2,15 @@ class Solution {
     public int maximalSquare(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[][] arr = new int[n][m];
+        int[][] arr = new int[n + 1][m + 1];
         int len = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == '0')
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (matrix[i - 1][j - 1] == '0')
                     continue;
 
-                arr[i][j] = Math.max(1, arr[i][j]);
-
-                int min = Integer.MAX_VALUE;
-                boolean flag = true;
-                if (i - 1 >= 0 && arr[i - 1][j] > 0)
-                    min = Math.min(min, arr[i - 1][j]);
-                else
-                    flag = false;
-                if (j - 1 >= 0 && arr[i][j - 1] > 0)
-                    min = Math.min(min, arr[i][j - 1]);
-                else
-                    flag = false;
-                if (i - 1 >= 0 && j - 1 >= 0 && arr[i - 1][j - 1] > 0)
-                    min = Math.min(min, arr[i - 1][j - 1]);
-                else
-                    flag = false;
-
-                arr[i][j] += min != Integer.MAX_VALUE && flag ? min : 0;
+                int min = Math.min(Math.min(arr[i - 1][j - 1], arr[i - 1][j]), arr[i][j - 1]);
+                arr[i][j] = 1 + min;
                 len = Math.max(len, arr[i][j]);
                 // 3 / 3 = 1 
                 // 6 / 3 = 2
