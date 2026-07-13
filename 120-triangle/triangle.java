13925f38383a1180d3e7bc37a63cdeba80624e52
Shiqi Pang <shiqipam@gmail.com>
2026-07-13 10:45:54 -0700

    Time: 3 ms (81.11%) | Memory: 45.2 MB (85.22%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -7,23 +7,13 @@ class Solution {
 
         for (int h = height - 1; h >= 0; h--) {
             List<Integer> row = triangle.get(h);
-                System.out.println("--");
             for (int i = 0; i < row.size(); i++) {
                 int min = Math.min(minSumOfIndex[i], minSumOfIndex[i + 1]);
                 minSumOfIndex[i] = min + row.get(i);
-                System.out.print(minSumOfIndex[i] + ",");
             }
         }
         return minSumOfIndex[0];
-        /**
-        minSumOfIndex
-        0, 0, 0, 0, 0
-        4, 1, 8, 3
-        7, 6, 10
-        3 +6， 4 + 6
-        9，10
-       11
-         */
+
 
     }
 }
