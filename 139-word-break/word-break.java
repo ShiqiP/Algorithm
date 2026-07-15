eee31f80a4e6f3174f7c05d195d19f7a4e675d9f
Shiqi Pang <shiqipam@gmail.com>
2026-07-15 15:06:45 -0700

    Time: 14 ms (5.02%) | Memory: 46.3 MB (36.47%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if ((i == 0 || dp[i]) && words.contains(s.substring(i, j))) {
+                    dp[j] = true;
+                    // break;
+                }
+            }
+        }
+        for(boolean a : dp){
+            System.out.print(a + ",");
+        }
+        return dp[n];
+    }
+}
