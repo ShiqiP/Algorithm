2c78a6990bfd91f94f9d37cffb9c1d9b48d3748f
Shiqi Pang <shiqipam@gmail.com>
2026-07-22 15:28:33 -0700

    Time: 4 ms (1.15%) | Memory: 46.7 MB (34.66%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -13,13 +13,37 @@
  *     }
  * }
  */
+class Pair {
+    int depth;
+    TreeNode node;
+
+    Pair(TreeNode node, int depth) {
+        this.depth = depth;
+        this.node = node;
+    }
+}
+
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
+        if (root == null)
+            return 0;
 
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
+        int ans = 0;
 
-        return Math.max(left, right) + 1;
+        Stack<Pair> stack = new Stack<>();
+
+        stack.add(new Pair(root, 1));
+
+        while(!stack.isEmpty()){
+            Pair pair = stack.pop();
+            TreeNode node = pair.node;
+
+            if(pair.node.left != null) stack.push(new Pair(node.left, pair.depth + 1));
+            if(pair.node.right != null) stack.push(new Pair(node.right, pair.depth + 1));
+
+            ans = Math.max(ans, pair.depth);
+        }
+
+        return ans;
     }
 }
