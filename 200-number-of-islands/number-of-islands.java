ca5bf29de37b3504429aa0a1030b03d2e6a97896
Shiqi Pang <shiqipam@gmail.com>
2026-07-22 23:49:06 -0700

    Time: 4 ms (46.49%) | Memory: 52.4 MB (46.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,40 +1,38 @@
 class Solution {
-    int[][] directions = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
-    int n, m;
+    boolean[][] visited;
+    int m;
+    int n;
 
     public int numIslands(char[][] grid) {
-        // to calculate the number of connected components;
-        // we can not access neighbors directlt from the input
-        // noticed that the neighbors is from the top/bottom/left/right;
-        n = grid.length;
-        m = grid[0].length;
-        boolean[][] seen = new boolean[n][m];
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && !seen[i][j]) {
+        // dfs to traverse all the islands
+        this.m = grid.length;
+        this.n = grid[0].length;
+        this.visited = new boolean[m][n];
+
+        for (int i = 0; i < this.m; i++) {
+            for (int j = 0; j < this.n; j++) {
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    dfs(i, j, grid);
                     ans++;
-                    dfs(i, j, seen, grid);
                 }
             }
         }
         return ans;
     }
 
-    public boolean isValid(int row, int col) {
-        return col >= 0 && col < m && row >= 0 && row < n;
-    }
-
-    public void dfs(int row, int col, boolean[][] seen, char[][] grid) {
-        // all the neighbors of node
-        seen[row][col] = true;
-        for (int[] direc : directions) {
-            int x = row + direc[0], y = col + direc[1];
-    
-            if (isValid(x, y) && !seen[x][y] && grid[x][y] == '1') {
-                seen[x][y] = true;
-                dfs(x, y, seen, grid);
-            }
+    public void dfs(int i, int j, char[][] grid) {
+        // base cases
+        if (i < 0 || i >= this.m || j < 0 || j >= this.n || grid[i][j] == '0' || visited[i][j]) {
+            return;
         }
+
+        visited[i][j] = true;
+
+        // 4 directions
+        dfs(i + 1, j, grid);
+        dfs(i - 1, j, grid);
+        dfs(i, j + 1, grid);
+        dfs(i, j - 1, grid);
     }
 }
