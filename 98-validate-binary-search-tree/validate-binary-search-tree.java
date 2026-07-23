2b29aacefce33109289cd5557637fec3f2214694
Shiqi Pang <shiqipam@gmail.com>
2026-07-22 18:55:31 -0700

    Time: 46 ms (1.36%) | Memory: 46.8 MB (5.35%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -22,10 +22,10 @@ class Solution {
         if (root.left == null && root.right == null)
             return true;
 
-        // if (root.left != null && root.left.val >= root.val) 
-        //     return false;
-        // if (root.right != null && root.right.val <= root.val)
-        //     return false;
+        if (root.left != null && root.left.val >= root.val) 
+            return false;
+        if (root.right != null && root.right.val <= root.val)
+            return false;
 
 
         int leftMax = getMax(root.left);
