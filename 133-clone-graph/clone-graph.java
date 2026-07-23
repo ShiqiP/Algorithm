0e647fd7f70ee6e36392188fc902249a56a274ad
Shiqi Pang <shiqipam@gmail.com>
2026-07-23 00:14:41 -0700

    Time: 25 ms (78.83%) | Memory: 44.3 MB (59.77%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -19,45 +19,42 @@ class Node {
 */
 
 class Solution {
-
+    // set to track the node has been created
     Map<Integer, Node> map = new HashMap<>();
-
-    /**
-     * 
-
-     map 
-     1 -> Node(1, [])
-     2 -> Node(2, [1, ])
-     3 -> Node(3, [2, ])
-     4 -> Node(4, [])
-
-     */
 
     public Node cloneGraph(Node node) {
 
         if (node == null)
             return null;
-        
-        if(map.containsKey(node.val))
+
+        if (map.containsKey(node.val))
             return map.get(node.val);
 
         List<Node> neighbors = node.neighbors;
 
-        // List<Node> copyNeighbors = new ArrayList<>();
         Node copyNode = new Node(node.val);
-        // copyNode.neighbors = copyNeighbors;
 
-            map.put(node.val, copyNode);
-
+        map.put(node.val, copyNode);
 
         // dfs from node
         for (Node n : neighbors) {
             Node newNode = cloneGraph(n);
-           copyNode.neighbors.add(newNode);
+            copyNode.neighbors.add(newNode);
         }
 
-        // set to track the node has been created
         return copyNode;
 
     }
+
+    /**
+    * 
+    
+    map 
+    1 -> Node(1, [])
+    2 -> Node(2, [1, ])
+    3 -> Node(3, [2, ])
+    4 -> Node(4, [])
+    
+    */
+
 }
