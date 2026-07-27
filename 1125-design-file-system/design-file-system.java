94d5c7dfc9988c8ed698275f550c01288eb59da9
Shiqi Pang <shiqipam@gmail.com>
2026-07-27 15:55:44 -0700

    Time: 100 ms (48.25%) | Memory: 69.2 MB (21.68%) - LeetSync

--- 1125-design-file-system/design-file-system.java
+++ 1125-design-file-system/design-file-system.java
@@ -1,66 +1,81 @@
 class FileSystem {
-    Map<String, Integer> map;
 
-    // Map<String, Pair<Integer, String[]>> map;
+    // The TrieNode data structure.
+    class TrieNode {
+
+        String name;
+        int val = -1;
+        Map<String, TrieNode> map = new HashMap<>();
+
+        TrieNode (String name) {
+            this.name = name;
+        }
+    }
+
+    TrieNode root;
+
+    // Root node contains the empty string.
     public FileSystem() {
-        this.map = new HashMap<>();
+        this.root = new TrieNode("");
     }
 
     public boolean createPath(String path, int value) {
-        // map path value kv
 
-        // false
-        /**
-        
-        map.contains(path)
-        !map.contains(parentpath)
-        split the path with "/"
-        /leetcode  /leetcode/problems
-        
-        "/leetcode/problems/set"  - [leetcode, problems,set]
-        n - 1 level exist ??
-        /leetcode
-        /leetcode/problems
-         */
-        // already exists
-        if (this.map.containsKey(path) || path.equals("/") || path.isBlank()) {
+        // Obtain all the components
+        String[] components = path.split("/");
+
+        // Start "curr" from the root node.
+        TrieNode cur = root;
+
+        // Iterate over all the components.
+        for (int i = 1; i < components.length; i++) {
+
+            String currentComponent = components[i];
+
+            // For each component, we check if it exists in the current node's dictionary.
+            if (cur.map.containsKey(currentComponent) == false) {
+
+                // If it doesn't and it is the last node, add it to the Trie.
+                if (i == components.length - 1) {
+                    cur.map.put(currentComponent, new TrieNode(currentComponent));
+                } else {
+                    return false;
+                }
+            }
+
+            cur = cur.map.get(currentComponent);
+        }
+
+        // Value not equal to -1 means the path already exists in the trie.
+        if (cur.val != -1) {
             return false;
         }
 
-        String[] strarr = path.split("/");
-
-        if (strarr.length < 3) {
-            this.map.put(path, value);
-            return true;
-        } else {
-            // check parent path
-            int delimIndex = path.lastIndexOf("/");
-            String parent = path.substring(0, delimIndex);
-            if(!this.map.containsKey(parent)) return false;
-            // StringBuilder builder = new StringBuilder();
-            // for (int i = 1; i < strarr.length - 1; i++) {
-            //     builder.append("/");
-            //     builder.append(strarr[i]);
-
-            //     String parent = builder.toString();
-            //     System.out.println(parent);
-            //     if (!this.map.containsKey(parent)) {
-            //         return false;
-            //     }
-            // }
-            this.map.put(path, value);
-            return true;
-        }
+        cur.val = value;
+        return true;
     }
 
     public int get(String path) {
-        return this.map.getOrDefault(path, -1);
+
+        // Obtain all the components
+        String[] components = path.split("/");
+
+        // Start "curr" from the root node.
+        TrieNode cur = root;
+
+        // Iterate over all the components.
+        for (int i = 1; i < components.length; i++) {
+
+            String currentComponent = components[i];
+
+            // For each component, we check if it exists in the current node's dictionary.
+            if (cur.map.containsKey(currentComponent) == false) {
+                return -1;
+            }
+
+            cur = cur.map.get(currentComponent);
+        }
+
+        return cur.val;
     }
 }
-
-/**
- * Your FileSystem object will be instantiated and called as such:
- * FileSystem obj = new FileSystem();
- * boolean param_1 = obj.createPath(path,value);
- * int param_2 = obj.get(path);
- */
