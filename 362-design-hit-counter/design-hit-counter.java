1c87608b164df9d81a439c00c6e55e1d9eb7260a
Shiqi Pang <shiqipam@gmail.com>
2026-07-27 11:46:40 -0700

    Time: 1 ms (94.28%) | Memory: 43 MB (44.43%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.java
+++ 362-design-hit-counter/design-hit-counter.java
@@ -1,39 +1,26 @@
 class HitCounter {
-    Map<Integer, Integer> map;
 
+    Queue<Integer> queue;
     public HitCounter() {
-        // ascending map 
-        this.map = new TreeMap<>();
+        queue = new LinkedList<>();
     }
 
     public void hit(int timestamp) {
-        // store the hit timestamp
-        // data structure 
-        // int []
-        // map timestamp - number
-        // arr,  map, 
-        int number = 1 + this.map.getOrDefault(timestamp, 0);
-        this.map.put(timestamp, number);
+        queue.offer(timestamp);
     }   
 
     public int getHits(int timestamp) {
-        /**
-        1. calculate time frame (timestamp - 300, timestamp)
-        2. retrive the number of hit within this timeframe
-        3. for from start to end  O(n);
-         */
-        
-        int start = Math.max(0, timestamp - 300); // not included
-        int end = timestamp;
-
-        int count = 0;
-
-        for(int t : this.map.keySet()){
-            if( t > start && t <= end){
-                count += this.map.get(t);
+        // poll out the hit not in the timestamp
+        while(queue.size() > 0){
+            int diff = timestamp - queue.peek();
+            if(diff >= 300){
+                queue.poll();
+            }else{
+                break;
             }
         }
-        return count;
+        // return the size of the queue
+        return queue.size();
     }
 }
 
